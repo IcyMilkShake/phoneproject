4d601d12b6040b919249a78b088e4f89875e9428
MilkShake <sirapat.sermcheep@gmail.com>
2025-02-24 18:19:26 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static('uploads'));
 app.use(cookieParser());  // Middleware to parse cookies
-
-// Set up session middleware
-const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
 app.use(session({
