66f6b9ef1ba14c054cbf1b1e10c894d0a25d91fd
MilkShake <sirapat.sermcheep@gmail.com>
2025-03-28 11:55:21 +0700

    as

--- script.js
+++ script.js
@@ -85,14 +85,17 @@ window.onload = function () {
         { theme: "outline", size: "large" }
     );
 
+    const googleButton = document.getElementById("googlesignin");
+    if (googleButton) {
+        googleButton.addEventListener("click", function () {
+            console.log("Google Sign-In button clicked");
+            // Manually trigger the Google OAuth redirect for the button flow
+            window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=764440109211-519r5j9m6cfh1ovuiu0vujo0f2ufaldg.apps.googleusercontent.com&redirect_uri=https://pat.ipo-servers.net/oauth/callback&response_type=code&scope=email profile openid`;
+        });
+    }
+    
     google.accounts.id.prompt();
 };
-
-document.getElementById("googlesignin").addEventListener("click", function () {
-    console.log("CLICKKKKKKK")
-    // Manually trigger the Google OAuth redirect for the button flow
-    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=764440109211-519r5j9m6cfh1ovuiu0vujo0f2ufaldg.apps.googleusercontent.com&redirect_uri=https://pat.ipo-servers.net/oauth/callback&response_type=code&scope=email profile openid`;
-});
 
 
 submit.addEventListener('click', async () => {
